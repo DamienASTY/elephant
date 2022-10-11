0aced3a32da9460cf617583a24cfdcb5a15d7cea
200051 <200051@PCI42101>
2022-10-11 14:26:22 +0200

    Ajoutez des fichiers projet.

---
+++ elephant/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace elephant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Elephant e1 = new Elephant("Zazou", 2);
+            Elephant e2 = new Elephant("Tati", 45);
+            int cli;
+            cli = int.Parse(Console.ReadLine());
+            switch(cli)
+            {
+                case 1:
+                    Console.WriteLine(e1.attr());
+                    break;
+                case 2:
+                    Console.WriteLine(e2.attr());
+                    break;
+                case 3:
+                    Elephant temp = e1;
+                    e1 = e2;
+                    e2 = temp;
+                    Console.WriteLine(e1.attr());
+                    Console.WriteLine(e2.attr());
+                    break;
+            }
+            Console.WriteLine(e1);
+            Console.WriteLine(e2);
+            e2.ecouteMessage("haaaaa", e1);
+            e1.envoieMessage("Haha lol.2", e2);
+            Elephant.quiALesPlusGrosse(e1, e2);
+
+        }
+    }
+}
---
+++ elephant/elephant.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace elephant
+{
+    class Elephant
+    {
+        private string _nom;
+        private uint _tailleOreille;
+        public Elephant(string nom, uint tailleOreille)
+        {
+            this._nom = nom;
+            this._tailleOreille = tailleOreille;
+        }
+
+        public uint tailleOreil()
+        {
+            return this._tailleOreille;
+        }
+
+        public string attr()
+        {
+            return $"nom : ${this._nom}; tailleOreille : ${this._tailleOreille}";
+        }
+
+        public override string ToString()
+        {
+            return $"Brrrriiieeeuuuuu je suis {this._nom} et mes orreilles font {this._tailleOreille} mêtre";
+        }
+
+        public void ecouteMessage(string message, Elephant quiDit)
+        {
+            Console.WriteLine(this._nom + " a entendu un message \n" + quiDit._nom + " a dit : " + message);
+        }
+        public void envoieMessage(string message, Elephant quiRecoit)
+        {
+            quiRecoit.ecouteMessage(message, this);
+        }
+
+        public static int quiALesPlusGrosse(Elephant e1, Elephant e2)
+        {
+            if(e1._tailleOreille > e2._tailleOreille)
+            {
+                Console.WriteLine($"{e1._nom} à les plus grande oreilles");
+                return 0;
+            }
+            Console.WriteLine($"{e2._nom} à les plus grande oreilles");
+            return 0;
+
+        }
+    }
+}
